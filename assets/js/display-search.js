4d37d78c3377a20de54d56a087649845af7eb045
peter kim <peterkim1025@gmail.com>
2023-09-25 20:45:26 -0400

    first push

---
+++ assets/js/display-search.js
@@ -0,0 +1,24 @@
+var resultTextEl = document.querySelector('#result-text');
+var resultContentEl = document.querySelector('#result-content');
+var searchFormEl = document.querySelector('#search-form');
+
+function getParams() {
+  // Get the search params out of the URL (i.e. `?q=london&format=photo`) and convert it to an array (i.e. ['?q=london', 'format=photo'])
+
+}
+
+function printResults(resultObj) {
+
+}
+
+function searchApi(query, format) {
+
+}
+
+function handleSearchFormSubmit(event) {
+
+}
+
+searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+
+getParams();
